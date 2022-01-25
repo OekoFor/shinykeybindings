8febf2852127422a59a5dc1260217de50fe39954
fegue <felix.gnthr@gmail.com>
2022-01-25 14:56:48 +0100

    adds documentation

--- R/key_args_to_js_condition.R
+++ R/key_args_to_js_condition.R
@@ -2,15 +2,16 @@
 #' make js condition
 #'
 #' only for internal use
+#' @inheritParams kb_click
 key_args_to_js_condition <- function(key, modifier) {
   if (is.null(key)) stop("Function argument 'key' is empty with no default")
   if (!is.null(modifier)) {
-    if(!modifier %in% c(NULL, "ctrl", "alt", "shift")) stop("unknown modifier")
+    if (!modifier %in% c(NULL, "ctrl", "alt", "shift")) stop("unknown modifier")
   }
   if (!is.null(modifier)) {
     modkey <- glue::glue("event.{modifier}Key && ")
   } else {
-    modkey = ''
+    modkey <- ""
   }
   glue::glue("{modkey}event.key === '{key}'")
 }
