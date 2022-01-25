c65c7b9be7dbcd6981f846329711285aae859234
fegue <felix.gnthr@gmail.com>
2022-01-25 14:58:34 +0100

    more examples

--- inst/shinyexamples/kb_examples/server.R
+++ inst/shinyexamples/kb_examples/server.R
@@ -1,20 +1,49 @@
 
 library(shiny)
 
-# Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
 
 
-    # Click -------------------------------------------------------------------
-    # Add
-    observeEvent(input$add, {
-        new_value <- input$number + 1
-        updateNumericInput(session, "number", value = new_value)
-    })
-    # Substract
-    observeEvent(input$substract, {
-        new_value <- input$number - 1
-        updateNumericInput(session, "number", value = new_value)
-    })
+  # Click -------------------------------------------------------------------
+  val <- reactiveVal()
+  observeEvent(input$button_hi, {
+    val("Hi!")
+  })
+  observeEvent(input$button_hello, {
+    val("Hello!")
+  })
+  output$greeting <- renderText({
+    val()
+  })
+
+  # checkbox
+  # observeEvent(input$checkbox, {
+  #   updateCheckboxInput(session, "checkbox", value = !input$checkbox)
+  # })
+  output$checkboxvalue <- renderPrint(input$checkbox)
+
+  # Focus -------------------------------------------------------------------
+  output$text <- renderText({
+    input$textfield
+  })
+
+
+  # Event -------------------------------------------------------------------
+  observeEvent(input$plus, {
+    golem::print_dev("plus")
+    new_value <- input$number + 1
+    updateNumericInput(session, "number", value = new_value)
+  })
+  # Substract
+  observeEvent(input$minus, {
+    golem::print_dev("minus")
+    new_value <- input$number - 1
+    updateNumericInput(session, "number", value = new_value)
+  })
+
+
+  # Radio  ------------------------------------------------------------------
+  output$radio_value <- renderText({input$radios})
+
 
 })
