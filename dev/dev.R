ad745f0607131982e43834aecf3dfea061bab5fe
fegue <felix.gnthr@gmail.com>
2022-01-26 10:48:46 +0100

    more dev stuff

--- dev/dev.R
+++ dev/dev.R
@@ -12,6 +12,11 @@ use_r("kb_click")
 use_r("key_args_to_js_condition")
 
 
+
+# Package Setup -----------------------------------------------------------
+use_readme_rmd()
+
+
 # Git and Github ----------------------------------------------------------
 # For the use of Github, follow this Blogpost to properly setup your credentialmanager if you haven't already
 # https://blog.djnavarro.net/posts/2021-08-08_git-credential-helpers/#use-libsecret-credential-manager
