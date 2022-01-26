f51b241d0c6ccee9b4deda4b8af845f1cd3106a7
fegue <felix.gnthr@gmail.com>
2022-01-26 10:44:53 +0100

    updates documentation

--- R/keybindings.R
+++ R/keybindings.R
@@ -1,15 +1,31 @@
 #' Keybindings for Shiny Widgets
 #'
 #' @description
-#' This will be the description of all functions
+#' ### \emph{\strong{Overview of keybinding functions:}}
+#' The keybinding functions set keyboard shortcuts to allow interactions with different shiny widgets without using the mouse pointer.\cr
+#' They mainly provide wrappers around different javascript actions and may sometimes be used interchangeably.\cr
+#' Common usage is:
+#' \itemize{
+#'   \item \strong{kb_click():} Shortcut will perform a click action. Usually used with \code{\link[shiny]{actionButton}} or \code{\link[shiny]{checkbox}}
+#'   \item \strong{kb_event():} Creates a Shiny Input that can be used to trigger a reactive e.g. \code{\link[shiny]{observeEvent}}. Does not need a UI-element.
+#'   \item \strong{kb_focus():} Shortcut focuses a widget, usually a \code{\link[shiny]{textInput}} or \code{\link[shiny]{selectInput}}. See Details for more use cases.
+#'   \item \strong{kb_radio():} Shortcut sets the value of \code{\link[shiny]{radioButtons}}. Needs one keybinding per value. See Details for an alternative approach.
+#' }
+#'
 #' @param inputId inputID of the widget you want to click on
-#' @param key the keyvalue e.g. "a". See Details
+#' @param key the keyvalue e.g. "a". Mostly just whatever the keypress produces.\cr Some special keys are:
+#' \itemize{
+#' \item \strong{Enter/Return key}: type \code{'Enter'}
+#' \item \strong{Space Bar}: type a literal space \code{" "}
+#' \item see this page for more key values\cr \url{https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key/Key_Values}
+#' }
 #' @param modifier Modifier Key. Allowed values are "ctrl", "alt" (option or ⌥ on OS X) or "shift". Only one modifier is allowed.
 #' @name keybindings
 #' @details
+#' Almost all widgets can be focused using \code{kb_focus()}. Some widgets also allow to change values when focused using the arrow key. It is possible avoid \code{kb_radio()} by first focusing the widget and then using the arrow keys to toggle different choices.
+#'
+#'
 #' NOTE: Your keybindings may conflict with default shortcuts from your browser e.g. \code{ctrl + `+`} to magnify the page. \pkg{shinykeybindings} will prevent this and will only listen to your custom keybindings
-#' Other common keys you might want to use besides the usal alphanumerical keys:
-#' - 'Enter' for '\code{'Enter'}, \code{'↵'} or \code{'Return'}
 #'
 #' https://jsfiddle.net/4j54jqt2/4
 #'
@@ -87,10 +103,11 @@ kb_event <- function(inputId,
 
 #' @rdname keybindings
 #' @export
+#' @param value the value (or choice) that will be selected
 kb_radio <- function(inputId,
-                             key = NULL,
-                             modifier = NULL,
-                             value = NULL) {
+                     key = NULL,
+                     modifier = NULL,
+                     value = NULL) {
   if (is.null(value)) warning("Function argument 'value' is empty with no default")
 
   keypress_js_condition <- key_args_to_js_condition(key, modifier)
